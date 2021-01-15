203aa20891e2569445d3a0175c391580664b8344
lingjun.jlj <jlj_vip98@qq.com>
2021-01-15 18:12:26 +0800

    :bulb: 添加源码注释

--- src/main/java/org/apache/ibatis/executor/statement/RoutingStatementHandler.java
+++ src/main/java/org/apache/ibatis/executor/statement/RoutingStatementHandler.java
@@ -37,7 +37,7 @@ public class RoutingStatementHandler implements StatementHandler {
   private final StatementHandler delegate;
 
   public RoutingStatementHandler(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) {
-
+    // 根据 StatementType 创建不同的 StatementHandler
     switch (ms.getStatementType()) {
       case STATEMENT:
         delegate = new SimpleStatementHandler(executor, ms, parameter, rowBounds, resultHandler, boundSql);
